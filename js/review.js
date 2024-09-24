724b70fb2839479cd20586caef34206805b455c3
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-24 15:41:06 +0600

    create review projects

---
+++ js/review.js
@@ -0,0 +1,12 @@
+// console.log("review");
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const inputValue = document.getElementById('text-area').value
+    console.log(inputValue);
+    
+    const container = document.getElementById('review')
+    const p = document.createElement('p')
+    p.innerText = inputValue
+
+    container.appendChild(p)
+    
+})
